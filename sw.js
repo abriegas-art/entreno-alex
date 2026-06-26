9d6738d203a18e38137bb07d5d7e06e59c7eb15b
abriegas-art <abriegas@gmail.com>
2026-06-26 09:09:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Estrategia cache-first para HTML y assets CDN.
    Permite usar la app sin cobertura (sótano del gym, etc.).
 */
-const VERSION = 'entreno-alex-v10';
+const VERSION = 'entreno-alex-v11';
 const CORE = [
   './',
   './index.html',
@@ -11,7 +11,7 @@ const CORE = [
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
+  'https://unpkg.com/@babel/standalone@7.26.4/babel.min.js'
 ];
 
 self.addEventListener('install', (e) => {
